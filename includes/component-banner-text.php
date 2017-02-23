0de40450a40eda795a8a7442a5561aaa001f6cb7
Fredward <faandrade@gmail.com>
2017-02-23 17:35:28 -0500

    removes unnecessary css files, hides homepage banner CTA

--- includes/component-banner-text.php
+++ includes/component-banner-text.php
@@ -11,7 +11,7 @@ $background_color = get_sub_field('background_color');
     <!-- <h1><?php echo $text; ?></h1> -->
     <h1>In the next eighty years, 3,000 languages are expected to disappear.
       <span>We won't let that happen.</span>
-      <a href="donate">Stand with us</a>
+      <!-- <a href="donate">Stand with us</a> -->
     </h1>
   </div>
 <?php endif; ?>
